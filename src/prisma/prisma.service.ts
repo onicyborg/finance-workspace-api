380e09ca063d743a7c417f7e2538d8c71abc9491
Ace <akhmadfauzy40@gmail.com>
2026-02-24 15:53:52 +0700

    fix: prisma connection

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+  INestApplication,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    const connectionString = configService.get<string>('DATABASE_URL');
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set');
+    }
+
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    app.enableShutdownHooks();
+  }
+}
